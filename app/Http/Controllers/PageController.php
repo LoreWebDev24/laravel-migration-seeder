6a80bb66559cfc770060421376b8b5af791fad92
Lorenzo Del Re <136103234+LoreWebDev24@users.noreply.github.com>
2023-12-14 10:21:11 +0100

    add a comment

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -11,7 +11,7 @@ class PageController extends Controller
     {
         // CON LA WHERE:
 
-        // $trains = Train::where('in_partenza_oggi',true);
+        // $trains = Train::where('in_partenza_oggi',true); oppure Train::where('departure_time','>',now());
         // return view('guest.index', compact('trains'));
 
         // CON LA SELECT ALL :
